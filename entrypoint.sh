bcb7e3e75409f05e474d84d2863f4a87a9a80c5a
b4b4r07 <b4b4r07@gmail.com>
2020-02-13 00:26:23 +0900

    Add validation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
+repository="${INPUT_REPOSITORY}"
+number="${INPUT_NUMBER}"
+body="${INPUT_BODY}"
+
+if [[ ! ${repository} == */* ]]; then
+  echo "[ERROR] repository ${repository}: invalid" >&2
+  exit 1
+fi
+
+if [[ ! ${number} =~ ^[0-9]+$ ]]; then
+  echo "[ERROR] number ${number}: invalid" >&2
+  exit 1
+fi
+
+if [[ -z ${body} ]]; then
+  echo "[ERROR] body: should not be empty" >&2
+  exit 1
+fi
+
 github-comment \
-  --repository="${INPUT_REPOSITORY}" \
-  --number="${INPUT_NUMBER}" \
-  --body="${INPUT_BODY}"
+  --repository="${repository}" \
+  --number="${number}" \
+  --body="${body}"
+
+if (( ${?} == 0 )); then
+  echo "[INFO] successfully done"
+  exit 0
+fi
+
+exit 1
